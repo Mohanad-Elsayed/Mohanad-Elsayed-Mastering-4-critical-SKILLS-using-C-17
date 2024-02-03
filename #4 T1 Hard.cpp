47b88d95349a05541d423ecc17ad90ba2dc78c44
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-03 11:04:45 +0000

    Create #4 T1 Hard.cpp

---
+++ #4 T1 Hard.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int a,b,c,t = -1;
+	cin >> a >> b >> c;
+
+	// 4 lines of codes
+	t = a;
+	a = b;
+	b = c;
+	c = t;
+	cout << a << ' ' << b << ' ' << c;
+	return 0;
+}
