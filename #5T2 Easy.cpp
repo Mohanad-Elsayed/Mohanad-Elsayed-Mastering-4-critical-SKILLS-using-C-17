c7a1025c29dcc21d06808d110bf243d30dc859c8
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-04 22:54:00 +0000

    Create #5T2 Easy.cpp

---
+++ #5T2 Easy.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define llu long long int
+#define FOR(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+int main() { 
+	int x = 201, y = 25;
+	long double res = (x%y)/(double)y;
+	cout << res;
+	return 0;
+}
