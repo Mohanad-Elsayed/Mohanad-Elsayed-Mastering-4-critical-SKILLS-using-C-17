83daac594f673a7a72013ed4007f51a1539c41ea
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-04 22:55:48 +0000

    Create #5 T3 Easy.cpp

---
+++ #5 T3 Easy.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define llu long long int
+#define FOR(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+int main() { 
+	int x = 27, y = 12;
+	while (x > y) x-= y;
+	cout << x;
+	return 0;
+}
