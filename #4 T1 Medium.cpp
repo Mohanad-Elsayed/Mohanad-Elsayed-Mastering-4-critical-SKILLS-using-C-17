ab6e79f78863fe2f61728767209bd871b13f5509
Moha665 <158146104+Mohannad-Elsayed@users.noreply.github.com>
2024-02-05 00:09:25 +0000

    Create #4 T1 Medium.cpp

---
+++ #4 T1 Medium.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define llu long long int
+#define FOR(i,a,b) for (int i=(int)(a);i<(int)(b);i++)
+int main() { 
+	int x = 0, curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cin >> curr;
+	if (x < curr) x = curr;
+	cout << x;
+	return 0;
+}
